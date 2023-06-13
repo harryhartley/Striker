c337f35efd54b6561c5f60fb7d548d827105d3ed
harryhartley <hhartley94@gmail.com>
2023-06-13 14:57:39 +0100

    stage strike ux improvements

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -1,23 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
+import { useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import { useState } from "react";
 
+// Expected room config:
+// Ruleset: Game Name, Best of x?, Legal stages, counterpick stages, banned stages, 
+//          number of bans, winnerCharacterLocked?
+
 const Home: NextPage = () => {
-  // Expected room config:
-  // Game Name, 
-  // Ruleset: Legal stages, counterpick stages, banned stages, number of bans, winnerCharacterLocked?
+  const { user } = useUser()
+  
   const socketConfig = {
-    p1: 'hyhy',
+    p1: user?.username,
     p2: 'Jawdrop'
   }
   const roomConfig = {
     gameName: 'Lethal League Blaze',
-    legalStages: [],
+    bestOf: 3,
+    legalStages: [
+      {name: 'Subway', image: 'https://i.imgur.com/TQliTA8.png', width: 'X', height: 'Y'},
+      {name: 'Desert', image: 'https://i.imgur.com/RxkSs5N.png', width: 'X', height: 'Y'},
+      {name: 'Room', image: 'https://i.imgur.com/TB6KlRK.png', width: 'X', height: 'Y'},
+      {name: 'Outskirts', image: 'https://i.imgur.com/R5b6ZNq.png', width: 'X', height: 'Y'},
+      {name: 'Streets', image: 'https://i.imgur.com/gSxcB5k.png', width: 'X', height: 'Y'}
+    ],
     counterpickStages: [],
     bannedStages: [],
     numberOfBans: 2
   }
+
+  const [selectedStage, setSelectedState] = useState(0)
   
-  const [state, setState] = useState(3)
+  const [roomState, setRoomState] = useState(3)
   return (
     <>
       <div className="flex items-end space-y-2 pt-4 pb-6 md:space-y-5">
@@ -30,52 +44,48 @@ const Home: NextPage = () => {
 
       {/* Ask user who will be going first - us, them, or random
           Confirm correct settings config */}
-      {state === 0 && 
+      {roomState === 0 && 
         <div></div>
       }
       
       {/* Blind character selection */}
-      {state === 1 && 
+      {roomState === 1 && 
         <div></div>
       }
       
       {/* W picks character first, L picks character second */}
-      {state === 2 && 
+      {roomState === 2 && 
         <div></div>
       }
       
       {/* Stage striking */}
-      {state === 3 && 
+      {roomState === 3 && 
         <>
-          <h2 className="flex justify-center text-2xl font-bold leading-8 tracking-tight">Ban your choice of stages</h2>
+          <h2 className="flex justify-center pb-2 text-2xl font-bold leading-8 tracking-tight">{socketConfig.p1}: Ban 1 Stage</h2>
           <div className="grid gap-4">
-            <div className="relative text-center text-white">
-                <img className="h-auto max-w-full rounded-lg" src="https://i.imgur.com/RxkSs5N.png" alt=""/>
-                <div className=" absolute bottom-2 left-4">Width: X Height: Y</div>
+            <div className="relative text-white font-bold">
+                <img className="rounded-lg" src={roomConfig.legalStages[selectedStage]?.image} alt={roomConfig.legalStages[selectedStage]?.name}/>
+                <div className="text-3xl absolute bottom-2 left-4">{roomConfig.legalStages[selectedStage]?.name}</div>
+                <div className="text-3xl absolute bottom-2 right-4">Width: X Height: Y</div>
+                <button className="text-4xl bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">BAN</button>
             </div>
             <div className="grid grid-cols-5 gap-4">
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="https://i.imgur.com/TQliTA8.png" alt="Subway"/>
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="https://i.imgur.com/RxkSs5N.png" alt="Desert"/>
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="https://i.imgur.com/TB6KlRK.png" alt="Room"/>
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="https://i.imgur.com/R5b6ZNq.png" alt="Outskirts"/>
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="https://i.imgur.com/gSxcB5k.png" alt="Streets"/>
-                </div>
+                {roomConfig.legalStages.map((stage, idx) => 
+                  <img 
+                    key={`stage-${idx}`} 
+                    className={`rounded-lg ${selectedStage === idx ? 'border-4 border-red-600 rounded-xl' : ''}`} 
+                    src={stage.image} 
+                    alt={stage.name}
+                    onClick={() => setSelectedState(idx)}
+                  />
+                )}
             </div>
           </div>
         </>
       }
 
       {/* W bans stages, L picks stage */}
-      {state === 4 && 
+      {roomState === 4 && 
         <div></div>
       }
     </>
