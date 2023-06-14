f72eaece071b0f45fece2b152048494ce2df1cb2
harryhartley <hhartley94@gmail.com>
2023-06-14 15:14:46 +0100

    add character select and result reporting

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -14,59 +14,98 @@ const Home: NextPage = () => {
     p1: user?.username,
     p2: 'Jawdrop'
   }
-  const roomConfig = {
-    gameName: 'Lethal League Blaze',
-    bestOf: 3,
-    legalStages: [
-      {name: 'Subway', image: 'https://i.imgur.com/TQliTA8.png', width: 'X', height: 'Y'},
-      {name: 'Desert', image: 'https://i.imgur.com/RxkSs5N.png', width: 'X', height: 'Y'},
-      {name: 'Room', image: 'https://i.imgur.com/TB6KlRK.png', width: 'X', height: 'Y'},
-      {name: 'Outskirts', image: 'https://i.imgur.com/R5b6ZNq.png', width: 'X', height: 'Y'},
-      {name: 'Streets', image: 'https://i.imgur.com/gSxcB5k.png', width: 'X', height: 'Y'},
-      {name: 'Elevator', image: 'https://i.imgur.com/nRFtIK9.png', width: 'X', height: 'Y'},
-      {name: 'Factory', image: 'https://i.imgur.com/s4sNwrw.png', width: 'X', height: 'Y'},
-      {name: 'Pool', image: 'https://i.imgur.com/8xhB7na.png', width: 'X', height: 'Y'},
-      {name: 'Stadium', image: 'https://i.imgur.com/90N2Yr8.png', width: 'X', height: 'Y'},
-    ],
-    counterpickStages: [],
-    bannedStages: [
-      {name: 'Streets', image: 'https://i.imgur.com/gf0oKf8.png', width: 'X', height: 'Y'}
-    ],
-    numberOfBans: 2
-  }
   // const roomConfig = {
   //   gameName: 'Lethal League Blaze',
   //   bestOf: 3,
+  //   legalCharacters: [
+  //     {name: 'Candyman', image: 'https://i.imgur.com/fiIa0Wp.png'},
+  //     {name: 'Dice', image: 'https://i.imgur.com/hVHrBkE.png'},
+  //     {name: 'Doombox', image: 'https://i.imgur.com/JI4k9Wo.png'},
+  //     {name: 'Dust & Ashes', image: 'https://i.imgur.com/aabjDwX.png'},
+  //     {name: 'Grid', image: 'https://i.imgur.com/eAmPWSU.png'},
+  //     {name: 'Jet', image: 'https://i.imgur.com/9wRfyzA.png'},
+  //     {name: 'Latch', image: 'https://i.imgur.com/3UbnjBP.png'},
+  //     {name: 'Nitro', image: 'https://i.imgur.com/ZGD5PqH.png'},
+  //     {name: 'Raptor', image: 'https://i.imgur.com/XfzhISG.png'},
+  //     {name: 'Sonata', image: 'https://i.imgur.com/o6WcxWD.png'},
+  //     {name: 'Switch', image: 'https://i.imgur.com/o9r8z6j.png'},
+  //     {name: 'Toxic', image: 'https://i.imgur.com/s9xaYCu.png'},
+  //     {name: 'Random', image: 'https://i.imgur.com/mgwDy0f.png', default: true},
+  //   ],
   //   legalStages: [
   //     {name: 'Subway', image: 'https://i.imgur.com/TQliTA8.png', width: 'X', height: 'Y'},
   //     {name: 'Desert', image: 'https://i.imgur.com/RxkSs5N.png', width: 'X', height: 'Y'},
   //     {name: 'Room', image: 'https://i.imgur.com/TB6KlRK.png', width: 'X', height: 'Y'},
   //     {name: 'Outskirts', image: 'https://i.imgur.com/R5b6ZNq.png', width: 'X', height: 'Y'},
-  //     {name: 'Streets', image: 'https://i.imgur.com/gSxcB5k.png', width: 'X', height: 'Y'}
-  //   ],
-  //   counterpickStages: [
+  //     {name: 'Streets', image: 'https://i.imgur.com/gSxcB5k.png', width: 'X', height: 'Y'},
   //     {name: 'Elevator', image: 'https://i.imgur.com/nRFtIK9.png', width: 'X', height: 'Y'},
+  //     {name: 'Factory', image: 'https://i.imgur.com/s4sNwrw.png', width: 'X', height: 'Y'},
   //     {name: 'Pool', image: 'https://i.imgur.com/8xhB7na.png', width: 'X', height: 'Y'},
+  //     {name: 'Stadium', image: 'https://i.imgur.com/90N2Yr8.png', width: 'X', height: 'Y'},
   //   ],
+  //   counterpickStages: [],
   //   bannedStages: [
   //     {name: 'Streets', image: 'https://i.imgur.com/gf0oKf8.png', width: 'X', height: 'Y'}
   //   ],
   //   numberOfBans: 2
   // }
+  const roomConfig = {
+    gameName: 'Lethal League Blaze',
+    bestOf: 3,
+    legalCharacters: [
+      {name: 'Candyman', image: 'https://i.imgur.com/fiIa0Wp.png'},
+      {name: 'Dice', image: 'https://i.imgur.com/hVHrBkE.png'},
+      {name: 'Doombox', image: 'https://i.imgur.com/JI4k9Wo.png'},
+      {name: 'Dust & Ashes', image: 'https://i.imgur.com/aabjDwX.png'},
+      {name: 'Grid', image: 'https://i.imgur.com/eAmPWSU.png'},
+      {name: 'Jet', image: 'https://i.imgur.com/9wRfyzA.png'},
+      {name: 'Latch', image: 'https://i.imgur.com/3UbnjBP.png'},
+      {name: 'Nitro', image: 'https://i.imgur.com/ZGD5PqH.png'},
+      {name: 'Raptor', image: 'https://i.imgur.com/XfzhISG.png'},
+      {name: 'Sonata', image: 'https://i.imgur.com/o6WcxWD.png'},
+      {name: 'Switch', image: 'https://i.imgur.com/o9r8z6j.png'},
+      {name: 'Toxic', image: 'https://i.imgur.com/s9xaYCu.png'},
+      {name: 'Random', image: 'https://i.imgur.com/mgwDy0f.png', default: true},
+    ],
+    legalStages: [
+      {name: 'Subway', image: 'https://i.imgur.com/TQliTA8.png', width: 'X', height: 'Y'},
+      {name: 'Desert', image: 'https://i.imgur.com/RxkSs5N.png', width: 'X', height: 'Y'},
+      {name: 'Room', image: 'https://i.imgur.com/TB6KlRK.png', width: 'X', height: 'Y'},
+      {name: 'Outskirts', image: 'https://i.imgur.com/R5b6ZNq.png', width: 'X', height: 'Y'},
+      {name: 'Streets', image: 'https://i.imgur.com/gSxcB5k.png', width: 'X', height: 'Y'}
+    ],
+    counterpickStages: [
+      {name: 'Elevator', image: 'https://i.imgur.com/nRFtIK9.png', width: 'X', height: 'Y'},
+      {name: 'Pool', image: 'https://i.imgur.com/8xhB7na.png', width: 'X', height: 'Y'},
+    ],
+    bannedStages: [
+      {name: 'Streets', image: 'https://i.imgur.com/gf0oKf8.png', width: 'X', height: 'Y'}
+    ],
+    numberOfBans: 2
+  }
 
+  const [currentScore, setCurrentScore] = useState<[number, number]>([0, 0])
+  const [p1Character, setP1Character] = useState(roomConfig.legalCharacters.find(x => x.default === true))
+  const [p2Character, setP2Character] = useState(roomConfig.legalCharacters.find(x => x.default === true))
+  const [p1CharacterLocked, setP1CharacterLocked] = useState(false)
   const [selectedStage, setSelectedState] = useState(0)
   const [currentBans, setCurrentBans] = useState<number[]>([])
   
-  const [roomState, setRoomState] = useState(3)
+  const [roomState, setRoomState] = useState(1)
   return (
     <>
-      <div className="flex items-end space-y-2 pt-4 pb-6 md:space-y-5">
+      <div className="flex items-center space-y-2 pt-4 md:space-y-5">
+        <img className="flex justify-center flex-1 w-4 max-w-xs" src={p1Character?.image} alt={p1Character?.name}/>
         <h1 className="flex justify-center flex-1 md:leading-14 text-2xl font-extrabold leading-9 tracking-tight sm:text-3xl sm:leading-10 md:text-5xl">{socketConfig.p1}</h1>
         <h1 className="flex justify-center flex-1 md:leading-14 text-2xl font-extrabold leading-9 tracking-tight sm:text-3xl sm:leading-10 md:text-5xl">vs</h1>
         <h1 className="flex justify-center flex-1 md:leading-14 text-2xl font-extrabold leading-9 tracking-tight sm:text-3xl sm:leading-10 md:text-5xl">{socketConfig.p2}</h1>
+        <img className="flex justify-center flex-1 w-4 max-w-xs" src={p2Character?.image} alt={p2Character?.name}/>
       </div>
-      {/* Match information: Players, current score */}
-        <div></div>
+      <div className="flex items-end justify-center gap-4 space-y-2 pb-6 md:space-y-5">
+        <h1 className="md:leading-14 text-xl font-bold leading-9 tracking-tight sm:text-3xl sm:leading-10 md:text-5xl">{currentScore[0]}</h1>
+        <h1 className="md:leading-14 text-xl font-bold leading-9 tracking-tight sm:text-3xl sm:leading-10 md:text-5xl">-</h1>
+        <h1 className="md:leading-14 text-xl font-bold leading-9 tracking-tight sm:text-3xl sm:leading-10 md:text-5xl">{currentScore[1]}</h1>
+      </div>
 
       {/* Ask user who will be going first - us, them, or random
           Confirm correct settings config */}
@@ -76,24 +115,51 @@ const Home: NextPage = () => {
       
       {/* Blind character selection */}
       {roomState === 1 && 
-        <div></div>
-      }
-      
-      {/* W picks character first, L picks character second */}
-      {roomState === 2 && 
-        <div></div>
+        <>
+          <h2 className="flex justify-center pb-2 text-2xl font-bold leading-8 tracking-tight">{!p1CharacterLocked ? socketConfig.p1 : socketConfig.p2}: Pick your character</h2>
+          <div className="grid gap-4">
+            <button 
+              onClick={() => {!p1CharacterLocked ? setP1CharacterLocked(true) : setRoomState(roomState + 1)}}
+              className="text-4xl bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            >
+              LOCK IN
+            </button>
+            <div className="grid grid-cols-5 gap-4">
+                {roomConfig.legalCharacters.map((character, idx) => 
+                  <div 
+                    onClick={() => {!p1CharacterLocked ? setP1Character(character) : setP2Character(character)}} 
+                    className="relative" key={`character-${idx}`}
+                  >
+                    <img 
+                      // className={`rounded-lg ${p1Character === idx ? 'border-4 border-red-600 rounded-xl' : ''}`} 
+                      src={character.image} 
+                      alt={character.name}
+                    />
+                  </div>
+                )}
+            </div>
+          </div>
+        </>
       }
       
       {/* Stage striking */}
-      {roomState === 3 && 
+      {roomState === 2 && 
         <>
-          <h2 className="flex justify-center pb-2 text-2xl font-bold leading-8 tracking-tight">{socketConfig.p1}: Ban 1 Stage</h2>
+          <h2 className="flex justify-center pb-2 text-2xl font-bold leading-8 tracking-tight">{ (((currentBans.length - 1) % 4) + 4) % 4 === 0 || (((currentBans.length - 2) % 4) + 4) % 4 === 0 ? socketConfig.p1 : socketConfig.p2 }: Ban {currentBans.length % 2 === 0 || currentBans.length === roomConfig.legalStages.length - 2 ? '1 stage' : '2 stages'}</h2>
           <div className="grid gap-4">
             <div className="relative text-white font-bold">
                 <img className="rounded-lg" src={roomConfig.legalStages[selectedStage]?.image} alt={roomConfig.legalStages[selectedStage]?.name}/>
                 <div className="text-3xl absolute bottom-2 left-4">{roomConfig.legalStages[selectedStage]?.name}</div>
-                <div className="text-3xl absolute bottom-2 right-4">Width: X Height: Y</div>
-                <button onClick={() => setCurrentBans([...currentBans, selectedStage])} className="text-4xl bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">BAN</button>
+                <div className="text-3xl absolute bottom-2 right-4">Width: {roomConfig.legalStages[selectedStage]?.width} Height: {roomConfig.legalStages[selectedStage]?.height}</div>
+                <button 
+                  onClick={() => {
+                    setCurrentBans([...currentBans, selectedStage])
+                    // currentBans dispatch isn't seen yet, so ban length is still 1 behind
+                    if (currentBans.length === roomConfig.legalStages.length - 2) {
+                      setRoomState(roomState + 1)
+                    }
+                  }} 
+                  className="text-4xl bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">BAN</button>
             </div>
             <div className="grid grid-cols-5 gap-4">
                 {roomConfig.legalStages.map((stage, idx) => 
@@ -113,8 +179,61 @@ const Home: NextPage = () => {
         </>
       }
 
-      {/* W bans stages, L picks stage */}
+      {/* Wait for game result - both players must agree for room */}
+      {roomState === 3 && 
+        <>
+          <h2 className="flex justify-center pb-2 text-2xl font-bold leading-8 tracking-tight">Game {currentScore[0]+currentScore[1]+1} - Report Score</h2>
+          <div className="grid gap-4">
+            <div className="grid grid-cols-2 gap-4">
+              <button
+                onClick={() => {
+                  setCurrentScore([currentScore[0] + 1, currentScore[1]])
+                  if (currentScore[0] === Math.floor(roomConfig.bestOf / 2)) {
+                    setRoomState(6)
+                  } else { 
+                    setRoomState (roomState + 1)
+                  }
+                }}
+                className="text-4xl bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              >
+                {socketConfig.p1}
+              </button>
+              <button 
+                onClick={() => {
+                  setCurrentScore([currentScore[0], currentScore[1] + 1])
+                  if (currentScore[1] === Math.floor(roomConfig.bestOf / 2)) {
+                    setRoomState(6)
+                  } else { 
+                    setRoomState (roomState + 1)
+                  }
+                }}
+                className="text-4xl bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              >
+                {socketConfig.p2}
+              </button>
+            </div>
+            <div className="relative text-white font-bold">
+                {/* set the correct stage being played! */}
+                <img className="rounded-lg" src={roomConfig.legalStages[selectedStage]?.image} alt={roomConfig.legalStages[selectedStage]?.name}/>
+                <div className="text-3xl absolute bottom-2 left-4">{roomConfig.legalStages[selectedStage]?.name}</div>
+                <div className="text-3xl absolute bottom-2 right-4">Width: {roomConfig.legalStages[selectedStage]?.width} Height: {roomConfig.legalStages[selectedStage]?.height}</div>
+            </div>
+          </div>
+        </>
+      }
+      
+      {/* W picks character first, L picks character second */}
       {roomState === 4 && 
+        <div></div>
+      }
+
+      {/* W bans stages, L picks stage */}
+      {roomState === 5 && 
+        <div></div>
+      }
+
+      {/* If game is over, display final result (and store in database) */}
+      {roomState === 6 && 
         <div></div>
       }
     </>
