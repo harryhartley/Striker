b0485e68e9f49d6f782280d26379929379453222
harryhartley <hhartley94@gmail.com>
2023-07-25 13:18:15 +0100

    refresh!

--- src/components/site/Header.tsx
+++
@@ -1,7 +0,0 @@
-export const Header = () => {
-  return (
-    <header className='flex items-center justify-between py-10'>
-      <div>Striker</div>
-    </header>
-  )
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,18 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from 'next-themes'
-import Head from "next/head";
-import { SEO } from "~/components/site/SEO";
-import { LayoutWrapper } from "~/components/site/LayoutWrapper";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <ThemeProvider attribute='class' defaultTheme={'system'}>
-        <Head>
-          <meta content='width=device-width, initial-scale=1' name='viewport' />
-        </Head>
-        <SEO />
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-      </ThemeProvider>
-    </ClerkProvider>
-  )
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
