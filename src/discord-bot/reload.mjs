07573ffa5f74d5f349ca451a8759bb06fa69fdd1
harryhartley <hhartley94@gmail.com>
2023-08-12 23:12:28 +0100

    update reload command to include options

--- src/discord-bot/reload.mjs
+++ src/discord-bot/reload.mjs
@@ -7,6 +7,46 @@ const commands = [
   {
     name: "striker",
     description: "Create a new Striker room",
+    options: [
+      {
+        type: 6,
+        name: "opponent",
+        description: "The opponent to challenge",
+        required: false,
+      },
+      {
+        type: 3,
+        name: "ruleset",
+        description: "The ruleset to use",
+        required: false,
+        choices: [
+          {
+            name: "stadium",
+            value: "clkp7ja74000008ju9zmg06n6",
+          },
+          {
+            name: "uk",
+            value: "clkp7jt33000108ju86xxdl51",
+          },
+        ],
+      },
+      {
+        type: 3,
+        name: "bestof",
+        description: "The number of games to play",
+        required: false,
+        choices: [
+          {
+            name: "3",
+            value: "3",
+          },
+          {
+            name: "5",
+            value: "5",
+          },
+        ],
+      },
+    ],
   },
   {
     name: "cancel",
